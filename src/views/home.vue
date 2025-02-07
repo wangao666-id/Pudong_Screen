d80ad240bfc5f161e32584b2b47ead5f0103ea19
Alan <70063978+wangao666-id@users.noreply.github.com>
2025-02-07 15:35:49 +0800

    '样式调整

--- src/views/home.vue
+++ src/views/home.vue
@@ -6,97 +6,137 @@
  * @FilePath: \web-pc\src\pages\big-screen\view\home.vue
 -->
 <template>
-  <!-- <div id="index" ref="appRef" class="index_home" :class="{ pageisScale: isScale }"> -->
-  <ScaleScreen
-    :width="1920"
-    :height="1080"
-    class="scale-wrap"
-    :selfAdaption="$store.state.setting.isScale"
-  >
-    <div class="bg">
-      <dv-loading v-if="loading">Loading...</dv-loading>
-      <div v-else class="host-body">
-        <!-- 头部 s -->
-        <div class="d-flex  title_wrap">
-<!--          <div class="zuojuxing"></div>-->
-<!--          <div class="youjuxing"></div>-->
-<!--          <div class="guang"></div>-->
-          <div class="d-flex jc-center">
-            <div class="title">
-                <span>浦东共青团大数据</span>
+    <!-- <div id="index" ref="appRef" class="index_home" :class="{ pageisScale: isScale }"> -->
+    <ScaleScreen
+        :width="1920"
+        :height="1080"
+        class="scale-wrap"
+        :selfAdaption="$store.state.setting.isScale"
+    >
+        <div class="bg">
+            <dv-loading v-if="loading">Loading...</dv-loading>
+            <div v-else class="host-body">
+                <!-- 头部 s -->
+                <div class="d-flex  title_wrap">
+                    <div class="d-flex jc-center">
+                        <div class="title">
+                            <p class="title-text">浦东共青团大数据</p>
+                        </div>
+                    </div>
+<!--  -->
+                    <div class="flex" >
+                        <div class="item-bj" v-for="item in dataArr" :key="item.title">
+                            <p>{{item.title}}</p>
+                            <p class="title-text">{{item.num}} {{item.code}}</p>
+                        </div>
+
+                    </div>
+                    <div class="timers">
+                        <!--            {{ dateYear }} {{ dateWeek }} {{ dateDay }}-->
+                        <!--            <i-->
+                        <!--              class="blq-icon-shezhi02"-->
+                        <!--              style="margin-left: 10px"-->
+                        <!--              @click="showSetting"-->
+                        <!--            ></i>-->
+                    </div>
+
+                </div>
+                <!-- 头部 e-->
+                <!-- 内容  s-->
+                <router-view></router-view>
+                <!-- 内容 e -->
             </div>
-          </div>
-          <div class="timers">
-<!--            {{ dateYear }} {{ dateWeek }} {{ dateDay }}-->
-<!--            <i-->
-<!--              class="blq-icon-shezhi02"-->
-<!--              style="margin-left: 10px"-->
-<!--              @click="showSetting"-->
-<!--            ></i>-->
-          </div>
         </div>
-        <!-- 头部 e-->
-        <!-- 内容  s-->
-        <router-view></router-view>
-        <!-- 内容 e -->
-      </div>
-    </div>
-    <Setting ref="setting" />
-  </ScaleScreen>
-  <!-- </div> -->
+        <Setting ref="setting"/>
+    </ScaleScreen>
+    <!-- </div> -->
 </template>
 
 <script>
-import { formatTime } from "../utils/index.js";
-import Setting from "./setting.vue";
-import ScaleScreen from "@/components/scale-screen/scale-screen.vue";
-export default {
-  components: { Setting, ScaleScreen },
-  data() {
-    return {
-      timing: null,
-      loading: true,
-      dateDay: null,
-      dateYear: null,
-      dateWeek: null,
-      weekday: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+    import {formatTime} from "../utils/index.js";
+    import Setting from "./setting.vue";
+    import ScaleScreen from "@/components/scale-screen/scale-screen.vue";
+
+    export default {
+        components: {Setting, ScaleScreen},
+        data() {
+            return {
+                timing: null,
+                loading: true,
+                dateDay: null,
+                dateYear: null,
+                dateWeek: null,
+                weekday: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+                dataArr: [
+                    {
+                    title:'14-35岁青年',
+                    code:'名',
+                    num:1756900
+                },
+                    {
+                        title:'共青团员',
+                        code:'名',
+                        num:1756900
+                    },
+                    {
+                        title:'少先队员',
+                        code:'名',
+                        num:1756900
+                    },
+                    {
+                        title:'儿童团员',
+                        code:'名',
+                        num:1756900
+                    },
+                    {
+                        title:'团组织',
+                        code:'家',
+                        num:1756900
+                    },
+                    {
+                        title:'少先队大队',
+                        code:'个',
+                        num:326
+                    }
+                ],
+            };
+        },
+        filters: {
+            numsFilter(msg) {
+                return msg || 0;
+            },
+        },
+        computed: {},
+        created() {
+        },
+        mounted() {
+            this.timeFn();
+            this.cancelLoading();
+        },
+        beforeDestroy() {
+            clearInterval(this.timing);
+        },
+        methods: {
+            showSetting() {
+                this.$refs.setting.init();
+            },
+            timeFn() {
+                this.timing = setInterval(() => {
+                    this.dateDay = formatTime(new Date(), "HH: mm: ss");
+                    this.dateYear = formatTime(new Date(), "yyyy-MM-dd");
+                    this.dateWeek = this.weekday[new Date().getDay()];
+                }, 1000);
+            },
+            cancelLoading() {
+                let timer = setTimeout(() => {
+                    this.loading = false;
+                    clearTimeout(timer);
+                }, 500);
+            },
+        },
     };
-  },
-  filters: {
-    numsFilter(msg) {
-      return msg || 0;
-    },
-  },
-  computed: {},
-  created() {},
-  mounted() {
-    this.timeFn();
-    this.cancelLoading();
-  },
-  beforeDestroy() {
-    clearInterval(this.timing);
-  },
-  methods: {
-    showSetting() {
-      this.$refs.setting.init();
-    },
-    timeFn() {
-      this.timing = setInterval(() => {
-        this.dateDay = formatTime(new Date(), "HH: mm: ss");
-        this.dateYear = formatTime(new Date(), "yyyy-MM-dd");
-        this.dateWeek = this.weekday[new Date().getDay()];
-      }, 1000);
-    },
-    cancelLoading() {
-      let timer = setTimeout(() => {
-        this.loading = false;
-        clearTimeout(timer);
-      }, 500);
-    },
-  },
-};
 </script>
 
 <style lang="scss">
-@import "./home.scss";
+    @import "./home.scss";
 </style>
